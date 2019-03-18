2c8310e8ee97ffd6ac68fb67f94ed9332df9fc4d
Tim Wiesner <timothyjwiesner@gmail.com>
2019-03-18 13:02:10 -0500

    Initial commit

---
+++ OnTheMap/OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Tim Wiesner on 3/18/19.
+//  Copyright © 2019 Tim Wiesner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
