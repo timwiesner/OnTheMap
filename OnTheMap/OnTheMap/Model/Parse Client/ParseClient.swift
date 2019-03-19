09cae7e4491b10d8e0ce995f4e84facfa820a316
Tim Wiesner <timothyjwiesner@gmail.com>
2019-03-19 12:06:58 -0500

    Add func TaskForPostRequest

--- OnTheMap/OnTheMap/Model/Parse Client/ParseClient.swift
+++ OnTheMap/OnTheMap/Model/Parse Client/ParseClient.swift
@@ -64,4 +64,34 @@ class ParseClient {
         }
         task.resume()
     }
+    
+    class func taskForPOSTRequest<RequestType: Encodable, ResponseType: Decodable>(url: URL, responseType: ResponseType.Type, body: RequestType, completion: @escaping (ResponseType?, Error?) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue(ParseClient.applicationId, forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue(ParseClient.apiKey, forHTTPHeaderField: "X-Parse-REST-API-Key")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try! JSONEncoder().encode(body)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                let responseObject = try decoder.decode(ResponseType.self, from: data)
+                DispatchQueue.main.async {
+                    completion(responseObject, nil)
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+            }
+        }
+        task.resume()
+    }
+
 }
