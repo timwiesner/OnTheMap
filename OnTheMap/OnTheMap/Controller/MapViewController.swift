5f92bbaee9cfb9cf22753b2246c91f7ccdcf3197
Tim Wiesner <timothyjwiesner@gmail.com>
2019-03-28 13:36:26 -0500

    Save before redo

--- OnTheMap/OnTheMap/Controller/MapViewController.swift
+++ OnTheMap/OnTheMap/Controller/MapViewController.swift
@@ -7,17 +7,50 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
     
-//    var students = [StudentLocation]()
+    var students = [StudentLocation]()
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var locations = [MKPointAnnotation]()
+        
         _ = ParseClient.getStudentLocation() { students, error in
             Common.shared.studentLocation = students
-            print(students)
         }
+        
+        
+        
+        for student in students {
+            let location = MKPointAnnotation()
+            location.title = student.firstName + " " + student.lastName
+            location.subtitle = student.mediaURL
+            location.coordinate = CLLocationCoordinate2DMake(student.latitude, student.longitude)
+            locations.append(location)
+        }
+        print(locations)
+        mapView.addAnnotations(locations)
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is MKPointAnnotation else { return nil }
+        
+        let identifier = "Annotation"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView!.canShowCallout = true
+        } else {
+            annotationView!.annotation = annotation
+        }
+        return annotationView
     }
 }
+
